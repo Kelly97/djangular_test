5c16ba27041956907c113f9a8c0951730f790330
Kelly Aguilar <kaguilar@confianza.hn>
2022-05-16 16:16:15 -0600

    Angular componentes y modulos

--- angular/src/app/components/components.module.ts
+++ angular/src/app/components/components.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpErrorComponent } from './http-error/http-error.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ToolbarComponent,
+    SidenavComponent,
+    FooterComponent,
+    HttpErrorComponent
+  ],
   imports: [
     CommonModule
   ]
--- angular/src/app/layouts/layouts.module.ts
+++ angular/src/app/layouts/layouts.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+
+import { LayoutsRoutingModule } from './layouts-routing.module';
+import { LayoutsComponent } from './layouts.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { InternalErrorComponent } from './internal-error/internal-error.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LayoutsComponent,    
+    LoginComponent,
+    NotFoundComponent,
+    AccessDeniedComponent,
+    InternalErrorComponent    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    LayoutsRoutingModule
   ]
 })
 export class LayoutsModule { }
