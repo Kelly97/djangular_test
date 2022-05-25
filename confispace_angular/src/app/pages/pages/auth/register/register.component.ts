54fbf20f1a58e0c90ece8b9cc0b50c0841860314
Kelly Aguilar <kaguilar@confianza.hn>
2022-05-25 16:50:40 -0600

    KMAA register validation

--- confispace_angular/src/app/pages/pages/auth/register/register.component.ts
+++ confispace_angular/src/app/pages/pages/auth/register/register.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import icVisibility from '@iconify/icons-ic/twotone-visibility';
 import icVisibilityOff from '@iconify/icons-ic/twotone-visibility-off';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { fadeInUp400ms } from '../../../../../@vex/animations/fade-in-up.animation';
 import { StorageService } from 'src/app/services/storage.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { CustomValidators } from 'src/app/utilities/validators';
 
 @Component({
   selector: 'vex-register',
@@ -17,13 +18,52 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
-  form: FormGroup;
-
   inputType = 'password';
   visible = false;
 
   icVisibility = icVisibility;
   icVisibilityOff = icVisibilityOff;
+
+  form = new FormGroup({
+    first_name: new FormControl("", [Validators.required, CustomValidators.onlyChar()]),
+    last_name: new FormControl("", [Validators.required, CustomValidators.onlyChar()]),
+    username: new FormControl("", [Validators.required, Validators.minLength(4)]),
+    email: new FormControl("", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+    passwordConfirm: new FormControl("", Validators.required)
+  },
+    CustomValidators.mustMatch('password', 'passwordConfirm'));
+
+  client_validation_messages = {
+    'first_name': [
+      { type: 'required', message: 'El campo es requerido.' },
+      { type: 'onlyChar', message: 'Solo se admiten letras y espacios.' }
+    ],
+    'last_name': [
+      { type: 'required', message: 'El campo es requerido.' },
+      { type: 'onlyChar', message: 'Solo se admiten letras y espacios.' }
+    ],
+    'username': [
+      { type: 'required', message: 'El campo es requerido.' },
+      { type: 'minlength', message: 'El nombre de usuario debe contener al menos 4 caracteres.' },
+      { type: 'pattern', message: 'Solo se admiten letras, número y los caracteres @.+-_' },
+    ],
+    'email': [
+      { type: 'required', message: 'El campo es requerido.' },
+      { type: 'pattern', message: 'Debe ingresar un correo válido.' }
+    ],
+    'password': [
+      { type: 'required', message: 'El campo es requerido.' },
+      { type: 'minlength', message: 'Se deben ingresar al menos 6 caracteres.' },
+      { type: 'pattern', message: 'La contraseña debe contener al menos una mayúscula, una minúscula y un número.' }
+    ],
+    'passwordConfirm': [
+      { type: 'required', message: 'El campo es requerido.' },
+      { type: 'mustMatch', message: 'Las contraseñas no coinciden.' }
+    ],
+  }
+
+  server_validation_messages = {};
 
   constructor(private router: Router,
     private fb: FormBuilder,
@@ -32,16 +72,7 @@ export class RegisterComponent implements OnInit {
     private storageService: StorageService
   ) { }
 
-  ngOnInit() {
-    this.form = this.fb.group({
-      first_name: ['', Validators.required],
-      last_name: ['', Validators.required],
-      username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      passwordConfirm: ['', Validators.required],
-    });
-  }
+  ngOnInit() { }
 
   send() {
     this.router.navigate(['/']);
@@ -57,6 +88,10 @@ export class RegisterComponent implements OnInit {
             this.storageService.setUser(result.user);
             this.router.navigate(['/'])
           }
+        }, (error) => {
+          this.server_validation_messages = error;
+          this.form.markAllAsTouched();
+          console.log(error.status)
         });
     }
   }
